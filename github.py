a074a933f59df5de3b7849086c24b4ba2a43df09
Shagun <sshagunsodhani@gmail.com>
2014-07-29 01:51:27 +0530

    Added functions to fetch repo list and language list

--- github.py
+++ github.py
@@ -47,12 +47,12 @@ class github:
         self.url = "https://api.github.com"
         config=ConfigParser()
         config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config', 'config.cfg'))
-        key = config.get('github','key')
-    
+        self.key = config.get('github','key')
 
     def userinfo(self, uname):
         # print "shagun"
-        url = self.url + "/users/"+str(uname)
+        url = self.url + "/users/" + str(uname) + "?access_token=" + str(self.key)
+        # print url
         r = requests.get(url)
         if(r.status_code!=200):
             print "\nError in fetching user info from Github. Error code : "+str(r.status_code)
@@ -60,6 +60,45 @@ class github:
             print "URL was : "+str(url)
             print "\n"
         else:
-            print r.json
-            print type(r.json)
-
+            print r.json()
+    
+    def userlang(self, uname):
+        # print "shagun"
+        url = self.url + "/users/" + str(uname) + "/repos?access_token=" + str(self.key)
+        # print url
+        language = {}
+        r = requests.get(url)
+        if(r.status_code!=200):
+            print "\nError in fetching user repos from Github. Error code : "+str(r.status_code)
+            print "Error message was : "+str(r.json['message'])
+            print "URL was : "+str(url)
+            print "\n"
+            return -1
+        else:
+            repo = []
+            a = r.json()
+            for j in a:
+                if j['language'] not in language.keys():
+                    language[j['language']] = int(j['size'])
+                else:
+                    language[j['language']] = language[j['language']] + int(j['size'])
+            return language
+     
+    
+    def userrepo(self, uname):# print "shagun"
+        url = self.url + "/users/" + str(uname) + "/repos?access_token=" + str(self.key)
+        # print url
+        repo = []
+        r = requests.get(url)
+        if(r.status_code!=200):
+            print "\nError in fetching user repos from Github. Error code : "+str(r.status_code)
+            print "Error message was : "+str(r.json['message'])
+            print "URL was : "+str(url)
+            print "\n"
+            return -1
+        else:
+            repo = []
+            a = r.json()
+            for j in a:
+                repo.append(j['name'])
+            return repo
